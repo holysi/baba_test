65c5b8783e2f6180dc707d20c248a3f824ed78b9
Christa <christalenz800@gmail.com>
2025-06-02 17:44:19 +0800

    add deploy workflow

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,14 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   optimizeDeps: {
+//     exclude: ['lucide-react'],
+//   },
+// });
+// define js
 export default defineConfig({
+  base: '/baba_test/', // ← 這個是你 repo 名稱
   plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
-});
+})
